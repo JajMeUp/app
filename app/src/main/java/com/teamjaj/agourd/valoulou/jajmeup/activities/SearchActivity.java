429f5524b97c94187ef512d4b2f83e5ecdba728a
valoulou <valentin.chaigneau@gmail.com>
2017-11-15 20:14:31 -0500

    Share OK / Message OK

---
+++ app/src/main/java/com/teamjaj/agourd/valoulou/jajmeup/activities/SearchActivity.java
@@ -0,0 +1,28 @@
+package com.teamjaj.agourd.valoulou.jajmeup.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+
+/**
+ * Created by Valentin on 29/08/2016.
+ */
+public class SearchActivity extends Activity implements SearchView.OnQueryTextListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+    System.out.println("On ecrit");
+        return false;
+    }
+
+}
